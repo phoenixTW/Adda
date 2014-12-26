7d89fa4e03801fdcb820e2d4da46c9267ca1e803
Jai Prakash <jaiprak@thoughtworks.com>
2014-12-26 17:57:09 +0530

    join button is working now

--- scripts/insertJoinedTable.sql
+++
@@ -1,5 +0,0 @@
-insert into joinedTopics (userId, topic_id) values (1,2);
-insert into joinedTopics (userId, topic_id) values (2,1);
-insert into joinedTopics (userId, topic_id) values (2,3);
-insert into joinedTopics (userId, topic_id) values (1,3);
-
