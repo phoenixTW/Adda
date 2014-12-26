14bcd1fa01f3ae60147c4995e50f00f877f8cdc6
Kaustav Chakraborty <kaustav.ron@gmail.com>
2014-12-26 10:13:10 +0530

    all tests are passing and init DB fixed

--- shellCommands/initdb.sh
+++ shellCommands/initdb.sh
@@ -4,4 +4,5 @@ mkdir test/data
 node scripts/initializeDB.js test/data/adda.db
 sqlite3 test/data/adda.db < scripts/insertData.sql
 sqlite3 data/adda.db < scripts/insertNewTables.sql
+sqlite3 test/data/adda.db < scripts/insertNewTables.sql
 cp test/data/adda.db test/data/adda.db.backup
