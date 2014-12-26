3ff07e8d62ac4266eda0990371ccdcdc964b9bb9
unknown <tprashan@INTPRASHAN.corporate.thoughtworks.com>
2014-12-26 11:11:59 +0530

    js server is on testing

--- server.js
+++ server.js
@@ -1,36 +1,34 @@
-// var exec = require('child_process').exec;
+var exec = require('child_process').exec;
 
 
-// var sound = function(){
-// 	exec('echo hello',function(te_err, std__out, std__err){
-// 		console.log(std__err);
-// 	});
-// };
+var sound = function(){
+	exec("vlc  sound/alert.mp3",function(te_err, std__out, std__err){
+		console.log(std__err);
+	});
+};
 
-// var runTest = function(){
-// 	exec('npm run test',function(te_err, std__out, std__err){
-// 		console.log(std__err);
-// 		if(std__err){
-// 			sound();
-// 		}
-// 	})
-// }
+var runTest = function(){
+	exec('npm run test',function(te_err, std__out, std__err){
+		(std__err) && sound();
+		(!std__err) && console.log("update successfull");
+	})
+}
 
-// var pullcommit = function(){
-// 	runTest();
-// 	return "hiiiiiiiiii";
-// };
+var pullcommit = function(){
+	exec('git pull',function(te_err, std__out, std__err){
+		console.log(std__err);
+		// runTest();
+	})
+};
 
-// var server = function(){
-// 	exec('git ls-remote https://github.com/phoenixTW/Adda.git HEAD | cut -c1-40',
-// 		function(err, stdout, stderr){
-// 			exec('git rev-parse HEAD',function(error, std_out, std_err){
-// 				if(std_out!=stdout){
-// 					pullcommit();
-// 				}
-// 			})
-// 	});
-// 	return "12";
-// };
+var server = function(){
+	exec('git ls-remote https://github.com/phoenixTW/Adda.git HEAD | cut -c1-40',
+		function(err, stdout, stderr){
+			exec('git rev-parse HEAD',function(error, std_out, std_err){
+				(std_out!=stdout) && pullcommit();
+				(std_out==stdout) && console.log("up-todate");
+			})
+	});
+};
 
-// console.log(server());
+server();
